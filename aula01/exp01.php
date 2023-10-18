c82fa051712e481d8a4498b2c25db7bde9a9ace0
hubert-fernando <145489358+hubertfernando@users.noreply.github.com>
2023-10-18 14:21:01 -0300

    Add files via upload

--- aula01/exp01.php
+++ aula01/exp01.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>exp1</title>
 </head>
 <body>
-    <h1>teste alteraçao do githug</h1>
+    <h1>teste de alteracao do githug</h1>
     <?php
     echo"teste de php"
     ?>
