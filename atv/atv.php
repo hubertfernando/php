35010f9edfc162b55f35251bad19317d9fc64a91
hubert-fernando <145489358+hubertfernando@users.noreply.github.com>
2024-05-07 14:15:07 -0300

    Add files via upload

---
+++ atv/atv.php
@@ -0,0 +1,154 @@
+<?php
+echo "<br>";
+echo"ALUNO 1 =>";
+echo "<br>";
+echo "<br>";
+
+$aluno1 = array(
+    "nome"=>"Hubert Fernando", 
+    "endereco"=> "Rua Cruzeiro",
+    "cidade"=> "Pindai",
+    "estado"=> "Bahia",
+    "idade"=> 17,
+    "cpf"=> 10822671557);
+
+echo "NOME: <br>" .$aluno1['nome'];
+echo "<br>";
+echo "ENDEREÇO: <br>" .$aluno1['endereco'];
+echo "<br>";
+echo "CIDADE: <br>" .$aluno1['cidade'];
+echo "<br>";
+echo "ESTADO: <br>" .$aluno1['estado'];
+echo "<br>";
+echo "IDADE: <br>" .$aluno1['idade'];
+echo "<br>";
+echo "CPF: <br>" .$aluno1['cpf'];
+echo "<br>";
+echo "<br>";
+echo"ALUNO 2 =>";
+echo "<br>";
+$aluno2 = array(
+    "nome"=>"Juliana", 
+    "endereco"=> "Rua pi",
+    "cidade"=> "Pindai",
+    "estado"=> "Bahia",
+    "idade"=> 17,
+    "cpf"=> 3444443343);
+echo "<br>";
+echo "NOME: <br>" .$aluno2['nome'];
+echo "<br>";
+echo "ENDEREÇO: <br>" .$aluno2['endereco'];
+echo "<br>";
+echo "CIDADE: <br>" .$aluno2['cidade'];
+echo "<br>";
+echo "ESTADO: <br>" .$aluno2['estado'];
+echo "<br>";
+echo "IDADE: <br>" .$aluno2['idade'];
+echo "<br>";
+echo "CPF: <br>" .$aluno2['cpf'];
+echo "<br>";
+echo "<br>";
+echo"ALUNO 3 =>";
+echo "<br>";
+$aluno3 = array(
+    "nome"=>"Dudu", 
+    "endereco"=> "Rua paraiba",
+    "cidade"=> "guanambi",
+    "estado"=> "Bahia",
+    "idade"=> 17,
+    "cpf"=> 4500064323);
+echo "<br>";
+echo "NOME: <br>" .$aluno3['nome'];
+echo "<br>";
+echo "ENDEREÇO: <br>" .$aluno3['endereco'];
+echo "<br>";
+echo "CIDADE: <br>" .$aluno3['cidade'];
+echo "<br>";
+echo "ESTADO: <br>" .$aluno3['estado'];
+echo "<br>";
+echo "IDADE: <br>" .$aluno3['idade'];
+echo "<br>";
+echo "CPF: <br>" .$aluno3['cpf'];
+echo "<br>";
+echo "<br>";
+echo"PROFESSORES";
+echo "<br>";
+echo "<br>";
+echo"PROFESSOR 1 =>";
+echo "<br>";
+$professor1 = array(
+    "nome"=>"Silvia", 
+    "endereco"=> "Rua ccc",
+    "cidade"=> "Guanambi",
+    "estado"=> "Bahia",
+    "idade"=> 45,
+    "cpf"=> 3455506995
+
+);
+echo "<br>";
+echo "NOME: <br>" .$professor1['nome'];
+echo "<br>";
+echo "ENDEREÇO: <br>" .$professor1['endereco'];
+echo "<br>";
+echo "CIDADE: <br>" .$professor1['cidade'];
+echo "<br>";
+echo "ESTADO: <br>" .$professor1['estado'];
+echo "<br>";
+echo "IDADE: <br>" .$professor1['idade'];
+echo "<br>";
+echo "CPF: <br>" .$professor1['cpf'];
+echo "<br>";
+
+echo "<br>";
+echo "<br>";
+echo"PROFESSOR 2 =>";
+echo "<br>";
+$professor2 = array(
+    "nome"=>"Fabio", 
+    "endereco"=> "Rua nnn",
+    "cidade"=> "Guanambi",
+    "estado"=> "Bahia",
+    "idade"=> 47,
+    "cpf"=> 4500662343
+
+);
+echo "<br>";
+echo "NOME: <br>" .$professor2['nome'];
+echo "<br>";
+echo "ENDEREÇO: <br>" .$professor2['endereco'];
+echo "<br>";
+echo "CIDADE: <br>" .$professor2['cidade'];
+echo "<br>";
+echo "ESTADO: <br>" .$professor2['estado'];
+echo "<br>";
+echo "IDADE: <br>" .$professor2['idade'];
+echo "<br>";
+echo "CPF: <br>" .$professor2['cpf'];
+echo "<br>";
+echo "<br>";
+echo"PROFESSOR 3 =>";
+echo "<br>";
+echo "<br>";
+$professor3 = array(
+    "nome"=>"Daniel", 
+    "endereco"=> "Rua lll",
+    "cidade"=> "Guanambi",
+    "estado"=> "Bahia",
+    "idade"=> 37,
+    "cpf"=> 4050550506
+
+);
+echo "<br>";
+echo "NOME: <br>" .$professor3['nome'];
+echo "<br>";
+echo "ENDEREÇO: <br>" .$professor3['endereco'];
+echo "<br>";
+echo "CIDADE: <br>" .$professor3['cidade'];
+echo "<br>";
+echo "ESTADO: <br>" .$professor3['estado'];
+echo "<br>";
+echo "IDADE: <br>" .$professor3['idade'];
+echo "<br>";
+echo "CPF: <br>" .$professor3['cpf'];
+echo "<br>";
+?>
